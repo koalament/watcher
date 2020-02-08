0557e4e4e44590766db4bc5df94dad76a9a10eb5
koalament <koalament@gmail.com>
2020-02-08 14:08:01 +0330

    move on zmq

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const _ = require("lodash");
-const async = require("async");
+const zmq = require("zeromq");
 const hex_decoder = require("raw-transaction-hex-decoder");
 const bitcoin_rpc = require('node-bitcoin-rpc');
 const http = require('http');
 const express = require('express');
+
+const zmqSock = zmq.socket("sub");
+zmqSock.connect(process.env.BITCOIN_NODE_ZMQ_ADDRESS);
+zmqSock.subscribe("rawtx");
+
 const app = express();
 const consoleLogger = require("tracer").colorConsole({ level: "info" });
 const socket_app = http.createServer(function (req, res) {
@@ -12,8 +17,6 @@ const socket_app = http.createServer(function (req, res) {
 });
 bitcoin_rpc.init(process.env.BITCOIN_NODE_RPC_HOST, parseInt(process.env.BITCOIN_NODE_RPC_PORT), process.env.BITCOIN_NODE_RPC_USER, process.env.BITCOIN_NODE_RPC_PASSWORD);
 const io = require('socket.io').listen(socket_app);
-
-let last_mempool = [];
 
 function getTransaction(tx_id, callback) {
   consoleLogger.info(tx_id)
@@ -39,70 +42,6 @@ function getTransaction(tx_id, callback) {
   })
 }
 
-function loop() {
-  bitcoin_rpc.call('getrawmempool', [], (err, res) => {
-    if (err) {
-      consoleLogger.error(err);
-      consoleLogger.error("retry getrawmempool")
-      setTimeout(() => {
-        loop();
-      }, 1000);
-      return;
-    }
-    if (!res || !res.result) {
-      throw new Error("No data!");
-    }
-    const mem_pool = res.result;
-    if (last_mempool.length !== 0) {
-      const difference = _.xor(last_mempool, mem_pool);
-      if (difference.length > 0) {
-        consoleLogger.info(difference)
-        function fn(tx_id, cb) {
-          getTransaction(tx_id, (err, tx) => {
-            if (err) {
-              cb(err);
-
-              return;
-            }
-            let decodedTx = undefined;
-            try {
-              decodedTx = hex_decoder.decodeRawUTXO(tx.hex);
-            } catch (e) {
-              consoleLogger.error(e);
-            }
-            if (!decodedTx) {
-              cb(undefined);
-
-              return;
-            }
-            io.sockets.emit("tx:*", tx, decodedTx);
-            const s = new Buffer(decodedTx.outs[0].script.split(" ").pop(), 'hex').toString('utf8');
-            const first_space = s.indexOf(" ");
-            const label = s.substring(0, first_space);
-            if (!label || label.trim() === "") {
-              cb(undefined);
-
-              return;
-            }
-            io.sockets.emit(label, tx, decodedTx);
-            cb(undefined);
-          })
-        }
-        async.eachLimit(difference, 1, fn, (err) => {
-          if (err) {
-            consoleLogger.error(err);
-          }
-          consoleLogger.info("Finished transactions:D")
-        })
-      }
-    }
-    last_mempool = mem_pool;
-    setTimeout(() => {
-      loop();
-    }, parseInt(process.env.WATCH_MEMPOOL_INTERVAL));
-  })
-}
-
 io.sockets.on('connection', function (socket) {
   consoleLogger.info("CLIENTS ", io.engine.clientsCount)
 
@@ -112,6 +51,29 @@ io.sockets.on('connection', function (socket) {
     consoleLogger.info('User disconnected');
   });
 });
+
+zmqSock.on('message', (topic, message) => {
+  if (topic.toString() === "rawtx") {
+    const hex = message;
+    let decodedTx = undefined;
+    try {
+      decodedTx = hex_decoder.decodeRawUTXO(hex);
+    } catch (e) {
+      consoleLogger.error(e);
+    }
+    if (!decodedTx) {
+      return;
+    }
+    io.sockets.emit("tx:*", { hex: hex }, decodedTx);
+    const s = new Buffer(decodedTx.outs[0].script.split(" ").pop(), 'hex').toString('utf8');
+    const first_space = s.indexOf(" ");
+    const label = s.substring(0, first_space);
+    if (!label || label.trim() === "") {
+      return;
+    }
+    io.sockets.emit(label, { hex: hex }, decodedTx);
+  }
+})
 
 app.get('/transaction/:tx_id', function (req, res, next) {
   getTransaction(req.params.tx_id, (err, transaction) => {
@@ -131,7 +93,3 @@ socket_app.listen(parseInt(process.env.SOCKET_PORT), process.env.HOST, () => {
 app.listen(parseInt(process.env.EXPRESS_PORT), process.env.HOST, () => {
   consoleLogger.info(`App is listening on port ${process.env.EXPRESS_PORT}`);
 });
-
-setTimeout(() => {
-  loop();
-}, 2000);
